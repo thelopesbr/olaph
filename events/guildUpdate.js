a1d1eb94e53c5774006863de89260a427f281b87
thelopesbr <jefersonlopes.sjn@hotmail.com>
2022-07-09 23:01:05 -0300

    :construction: evento guildUpdate funcional

--- events/guildUpdate.js
+++ events/guildUpdate.js
@@ -3,14 +3,14 @@ const EmbedInfo = require("../components/info");
 module.exports = {
 	name: 'guildUpdate',
 	once: false,
-	execute: async function(client) {
+	execute: async function(activator,client,prefix) {
 		try{
-			const config = Config(client);
-			const { cor} = config
+			const { cor, info } = Config(client);
+	
 			const embedInfo = EmbedInfo(client, cor);
+			const panel = await client.channels.cache.get(info.channel).messages.fetch(info.panel);
 		
-			const msg = await client.channels.cache.get('875200645626478605').messages.fetch('878784360193622046');
-			msg.edit({embeds: [embedInfo]})
+			panel.edit({embeds: [embedInfo]});
 		}
 		catch (err) {
 			console.error(err)
