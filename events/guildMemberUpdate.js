4790b247f5817ff3d197e92027ff22c138c88d65
thelopesbr <jefersonlopes.sjn@hotmail.com>
2022-08-19 17:27:37 -0300

    :recycle: Refatorando a atualização dos cargos  para utilizar o mongodb

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -11,19 +11,39 @@ module.exports = {
 	execute: async function(member,client) {
 		try{
 			const config = Config(client);
-			const { cor } = config
+			const { cor, info, servidor, consoleServer } = config
 			const embedInfo = EmbedInfo(client, cor);
-			const cargos = []
 			const user = await Member.findOne({tag: member.user.tag},'id name tag cargos')
-			
-			client.guilds.cache.get('875200644678570075').members.cache.get(member.id).roles.cache.map(role => {
-				if(role.name != '@everyone'){
-					cargos.push(role.id)
-				}
-			})
-			user.assign({cargos: cargos}).write()
-			const msg = await client.channels.cache.get('875200645626478605').messages.fetch('878784360193622046');
-			msg.edit({embeds: [embedInfo]});
+			if(user){
+				newCargos = []
+				client.guilds.cache.get(servidor.id).members.cache.get(member.id).roles.cache.map(role => {
+					if (role.name != '@everyone'){
+						newCargos.push(role.id)
+					}
+				})
+				user.updateOne(
+					({
+					$set: {
+						cargos: newCargos
+					}
+				}))
+				.then(() => {
+					consoleServer.send(`<:update:1010280091680518228>  Os cargos do membro **${member.user.tag}** foram atualizados.`
+					)})
+				.catch(error => {
+					const embed = new MessageEmbed()
+						.setTitle('Relatorio de erro gerado pela API:')
+						.setDescription(error.stack)
+						.setColor('#FF0000')
+					consoleServer.send({embeds: [embed]}); 
+				})
+			}
+			else{
+				consoleServer.send(`⚠️ O membro **${member.user.tag}** atualizou os cargos, mas o mesmo não está cadastrado no sistema.`)
+
+			}
+			const panel = await client.channels.cache.get(info.channel).messages.fetch(info.panel);
+			panel.edit({embeds: [embedInfo]});
 		}
 		catch (err) {
 			console.error(err)
